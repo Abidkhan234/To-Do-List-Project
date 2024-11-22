7822454d46086a2716c59a535fedc5166e5807eb
Abidkhan234 <abidshahkhan237@gmail.com>
2024-11-22 17:02:58 +0500

    Change everything

--- app.js
+++ app.js
@@ -1,26 +1,46 @@
-var input = document.getElementById("input");
-
 var listContainer = document.getElementById("list-container");
 
-function addTask() {
-    if (input.value === "") {
-        alert("You must write something")
-    } else {
-        let li = document.createElement("li");
-        li.innerHTML = input.value;
-        listContainer.appendChild(li);
+var input = document.getElementById("input-box");
 
-        let span = document.createElement("span");
-        span.innerHTML = "\u00d7";
-        li.appendChild(span);
-    }
-    input.value = "";
-} 
+var inputBtn = document.getElementById("input-btn");
 
-listContainer.onclick = (e) =>{
-    if(e.target.tagName === "LI"){
-        e.target.classList.toggle("active");
-    }else if(e.target.tagName === "SPAN"){
-        e.target.parentElement.remove(); 
-    }
+var ind = null;
+
+var arr = [];
+
+var renderList = () => {
+    listContainer.innerHTML = "";
+    arr.forEach((value, index) => {
+        listContainer.innerHTML += `<li><p id='li'>${value}</p> <button id = 'delete' onclick = 'remove(${index})'>Remove</button> <button id = 'edit'  onclick = 'edit(${index})'>Edit</button></li>`;
+    });
 }
+
+var addTask = () => {
+    arr.unshift(input.value);
+    input.value = "";
+    renderList();
+}
+
+
+var remove = (i) => {
+
+    arr.splice(i, 1);
+
+    renderList();
+}
+
+var edit = (i) => {
+    input.value = arr[i];
+    document.getElementById("input-btn2").style.display = "inline";
+    inputBtn.style.display = "none";
+    ind = i;
+}
+
+var UpdateToDoItem = () => {
+    if (!input.value) return;
+    arr[ind] = input.value;
+    input.value = "";
+    inputBtn.style.display = "inline";
+    document.getElementById("input-btn2").style.display = "none";
+    renderList();
+};
